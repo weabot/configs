12c3ed268288b3a508abd4fbc76b97ea13d6e8da
weabot <weabot@yandex.ru>
2017-06-19 20:57:58 -0400

    aa

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -11,6 +11,8 @@ static const char normfgcolor[]     = "#bbbbbb";
 static const char selbordercolor[]  = "#005577";
 static const char selbgcolor[]      = "#005577";
 static const char selfgcolor[]      = "#eeeeee";
+static unsigned int baralpha        = 0xd0;
+static unsigned int borderalpha     = OPAQUE;
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -56,8 +58,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "konsole", NULL };
-static const char *lockcmd[]  = { "slock", NULL };
+static const char *termcmd[]  = { "terminal", NULL }; // I decided to just symlink my preferred terminal to "terminal"
+static const char *lockcmd[]  = { "locker", NULL }; // Same with screen locker
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
